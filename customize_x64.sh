5ca03b0d822471a8810e51f825bcf371d4a409e7
yasum2006 <32138091+yasum2006@users.noreply.github.com>
2020-01-15 11:01:17 +0800

    Update customize_x64.sh

--- customize_x64.sh
+++ customize_x64.sh
@@ -7,3 +7,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.8.222/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/GEN8/g' openwrt/package/base-files/files/bin/config_generate
